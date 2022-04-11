175943c4785be0265068ae8a900a0d0fd6cd8626
Tiffany <twarre30@gmail.com>
2022-04-11 13:36:06 -0500

    still try to connect'

--- villagers.js
+++ villagers.js
@@ -1,36 +1,13 @@
-const animals = document.querySelector('#animals')
-const spinner = document.querySelector(".spinner")
+const $animals = document.querySelector('#animals')
+const $villagerName =document.querySelector('.villagerName')
 
-
-function addVillagersImage(villagers) {
-    const div = document.createElement('div')
-    div.classList.add('villager-listing')
-    div.innerHTML = `
-        <figure>
-            <img src='https://acnhapi.com/v1/images/villagers/' alt='$villagers.name' />
-            <figcaption class= "imageName"><a href="villagers.html?villagers=${villagers.name}">${villagers.name}</a></figcaption>
-        </figure>      
-    `
-    app.append(div)
-}
-
-const url = 'http://acnhapi.com/v1/villagers'
+const url = 'http://acnhapi.com/v1/villagers/1'
 fetch(url)
     .then(response => {
         return response.json()
     }).then(response => {
-        const urls = response.results.map(result => result.url)
-        const fetches = urls.map(url => fetch(url).then(response => response.json()))
-        return Promise.all(fetches) 
-        console.log(fetches)
-    }).then(responses => {
-        spinner.classList.add('hidden')
-        responses.forEach(response => {
-            addVillagersImage(response)
-        })
-            .catch((error) => {
-                const $p = document.createElement('p');
-                $p.textContent = "Something went wrong!";
-                document.querySelector('#app').append($p);
-            })
+        const villageUrl = response.name.name-USen
+        const li = document.querySelector('li')
+        $villagerName.append(li)
+        console.log(villageUrl)
     })
