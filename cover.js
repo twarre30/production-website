555f9a8b986ff026e2dbd868c20b983b18ea9dd6
Tiffany <twarre30@gmail.com>
2022-04-19 22:10:44 -0500

    Receive results

--- cover.js
+++ cover.js
@@ -1,31 +1,7 @@
-/*
-const queryParameters = new URLSearchParams(window.location.search)
-const resultsList = document.querySelector('results')
-const villager = queryParameters.get('villager')
+const resultsList = document.querySelector('#results')
+    new URLSearchParams(window.location.search).forEach((value, name) => {
+        resultsList.append(`Your ${name} is ${value}`)
+        resultsList.append(document.createElement('br'))
+    })
 
 
-const url = `https://acnhapi.com/v1a/villagers/${villager}`
-fetch(url)
-    .then((response) => response.json())
-    .then((villager) => {
-        const $img = document.createElement('img');
-        $img.src = villager.icon_uri;
-        resultsList.append($img);
-    });
-    */
-        const resultsList = document.querySelector('#results')
-        new URLSearchParams(window.location.search).forEach((value, name) => {
-            resultsList.append(`${name}: ${value}`)
-            resultsList.append(document.createElement('br'))
-        })
-
-
-
-    /*
-    
-        .forEach((value, name) => {
-            resultsList.append(`${name}: ${value}`)
-            resultsList.append(document.createElement('br'))
-        })
-
-        */
