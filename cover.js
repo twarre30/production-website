8acb2e43730ec984490164e60592185625fed9c5
Tiffany <twarre30@gmail.com>
2022-04-24 21:58:28 -0500

    update cover and removed a button

--- cover.js
+++ cover.js
@@ -1,9 +1,17 @@
-const resultsList = document.querySelector('#results')
-new URLSearchParams(window.location.search).forEach((value, name) => {
-    resultsList.append(`Your ${name} is ${value}`)
-    resultsList.append(document.createElement('br'))
-}).catch((error) => {
+const resultsList = document.querySelector('#results');
+const params = new URLSearchParams(window.location.search);
+const firstName = params.get("Name");
+const animal = params.get("Animal")
+const greeting = document.createElement("p")
+    .innerHTML = `
+    Welcome ${firstName}.
+    Your chosen animal is ${animal}. 
+    `
+console.log(animal)
+resultsList.append(greeting)
+/*
+.catch((error) => {
     window.location.href = '404page.html'
-})
+}) */
 
 
