71021f36785b34cf6ce3956d254f441a5b49bf9e
Tiffany <twarre30@gmail.com>
2022-04-24 22:07:32 -0500

    reinstated 404

--- cover.js
+++ cover.js
@@ -4,14 +4,14 @@ const firstName = params.get("Name");
 const animal = params.get("Animal")
 const greeting = document.createElement("p")
     .innerHTML = `
-    Welcome ${firstName}.
+    Welcome ${firstName}!
     Your chosen animal is ${animal}. 
     `
 console.log(animal)
 resultsList.append(greeting)
-/*
+
 .catch((error) => {
     window.location.href = '404page.html'
-}) */
+})
 
 
