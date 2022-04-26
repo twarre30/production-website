40e0220a251d67e87948ba9c3f736d62fbcb6364
Tiffany <twarre30@gmail.com>
2022-04-25 21:04:13 -0500

    event listerner is broke

--- cover.js
+++ cover.js
@@ -7,12 +7,43 @@ const greeting = document.createElement("p")
     Welcome ${firstName}!
     Your chosen animal is ${animal}. 
     `
-console.log(animal)
 resultsList.append(greeting)
 
 
-.catch ((error) => {
-    window.location.href = '404page.html'
+
+const $form = document.querySelector("form")
+const $main = document.querySelector("main")
+const reset = document.querySelector("reset")
+
+$form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let formData = new FormData(event.target);
+    let favoriteAnimal = formData.get("favorite-animal")
+    getFavoriteAnimal(favoriteAnimal);
 })
 
 
+function getFavoriteAnimal(favoriteAnimal) {
+    fetch('https://acnhapi.com/v1a/villagers')
+        .then(response => response.json())
+        .then(data => {
+                data.map(response => response)
+                    .filter(villager => villager.species === `${favoriteAnimal}`)
+            console.log(data)
+            const card = document.createElement("div")
+            card.innerHTML = `
+                <img src='${villager.image_uri}' alt='${villager.name['name-USen']}'/>
+                <figcaption>${villager.name['name-USen']}</figcaption >
+                <ul>
+                    <li> Personality: ${villager.personality}</li>
+                    <li> Birthday: ${villager['birthday-string']}</li>
+                    <li> Species: ${villager.species}</li>
+                    <li> Gender: ${villager.gender} </li>
+                    <li> Hobby:${villager.hobby} </li>
+                </ul>
+                `
+            $main.append(card)
+        }) .catch((error) => {
+            window.location.href = '404page.html'
+    })
+}
--- villagers.js
+++ villagers.js
@@ -1,5 +1,4 @@
-const villagerCardTemplate = document.querySelector('[data-villager-template]')
-const villagerCardContainer = document.querySelector('.villager-cards')
+const villagerCardContainer = document.querySelector('main')
 const spinner = document.querySelector('.lds-hourglass')
 const searchInput = document.querySelector('[data-search]')
 
@@ -10,13 +9,18 @@ searchInput.addEventListener('input', event => {
     villagers.forEach(villager => {
         const isVisible = villager.name.toLowerCase().includes(value) || villager.personality.toLowerCase().includes(value) ||
             villager.birthday.toLowerCase().includes(value) || villager.species.toLowerCase().includes(value) ||
-            villager.gender.toLowerCase().includes(value) || villager.hobby.toLowerCase().includes(value) 
+            villager.gender.toLowerCase().includes(value) || villager.hobby.toLowerCase().includes(value)
         villager.element.classList.toggle('hide', !isVisible)
     })
 })
 
-function createVillagerCard(villager) {
-    villagerCardContainer.innerHTML = `
+fetch('https://acnhapi.com/v1a/villagers')
+    .then(response => response.json())
+    .then(data => {
+        spinner.classList.add('hidden')
+        return data.map(villager => villager).forEach(villager => {
+            const card = document.createElement("div")
+            card.innerHTML = `
         <img src='${villager.image_uri}' alt='${villager.name['name-USen']}'/>
         <figcaption>${villager.name['name-USen']}</figcaption >
         <ul>
@@ -27,39 +31,7 @@ function createVillagerCard(villager) {
             <li> Hobby:${villager.hobby} </li>
         </ul>
     `
-}
-
-const url = 'https://acnhapi.com/v1a/villagers'
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        spinner.classList.add('hidden')
-        villagers = data.map(villager => {
-            createVillagerCard(villager)
-console.log(villager)
-            /*
-                        const card = villagerCardTemplate.content.cloneNode(true).children[0]
-                        const image = card.querySelector('[data-image]')
-                        const name = card.querySelector('[data-name]')
-                        const personality = card.querySelector('[data-personality]')
-                        const birthday = card.querySelector('[data-birthday')
-                        const species = card.querySelector('[data-species]')
-                        const gender = card.querySelector('[data-gender]')
-                        const hobby = card.querySelector('[data-hobby]')
-                        const phrase = card.querySelector('[data-phrase]')
-                        image.src = villager.image_uri
-                        name.textContent = villager.name['name-USen']
-                        personality.textContent = `Personality: ${ villager.personality } `
-                        birthday.textContent = `Birthday: ${ villager['birthday-string'] } `
-                        species.textContent = `Species: ${ villager.species } `
-                        gender.textContent = `Gender: ${ villager.gender } `
-                        hobby.textContent = `Hobby: ${ villager.hobby } `
-                        phrase.textContent = `Catch - Phrase: ${ villager['catch-phrase'] } `
-                        villagerCardContainer.append(card) */
-            return {
-                name: villager.name['name-USen'], personality: villager.personality, birthday: villager['birthday-string'], species: villager.species,
-                gender: villager.gender, hobby: villager.hobby
-            }
+            villagerCardContainer.append(card)
         })
     }).catch((error) => {
         window.location.href = '404page.html'
